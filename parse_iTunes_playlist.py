f3a4827ba500704accaf234da9e91d82d486fa96
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2019-02-16 16:04:51 -0800

    Parse playlist name

--- parse_iTunes_playlist.py
+++ parse_iTunes_playlist.py
@@ -9,12 +9,21 @@
 # written for Python 2.7.1
 #
 #
+import sys
 import xml.etree.ElementTree as e
 
 # GLOBAL FUNCTIONS
 def song_name(d):
 	# parse and return song name from a dict xml element 'd'
   return d[5].text + ' - ' + d[3].text
+
+def playlist_items(d):
+	# return list of songs in a playlist
+	return [x[1].text for x in d[15]]
+
+def playlist_name(d):
+	# return name of playlist
+	return d[1].text
 
 # GLOBAL CONSTANTS
 # Replace this with the path to your file
@@ -29,16 +38,40 @@ NOTE: This is the number of lines after the first dict node
 """
 XML_LINE_WHERE_TRACKS_START = 17
 
-# Start here
-xml_file = e.parse(FILEPATH).getroot()
+# Read in file
+try:
+	xml_file = e.parse(FILEPATH).getroot()
+	dict_contents = [_ for _ in xml_file[0]]
+except:
+	sys.exit("Could not read the xml file. Check the value of FILEPATH and the file itself")
 
-dict_contents = [x for x in xml_file[0]]
-inner_dict_contents = dict_contents[XML_LINE_WHERE_TRACKS_START]
+# Parse songs
+print "Attempting to parse songs...",
+try:
+	song_dict_contents = dict_contents[XML_LINE_WHERE_TRACKS_START]
+	SONGS = {}	# key-value map of song integer to its full name
 
-SONGS = {}	# key-value map of song integer to its full name
+	for n in range(len(song_dict_contents)/2):
+	  SONGS[song_dict_contents[2*n].text] = song_name(song_dict_contents[2*n + 1])
 
-for n in range(0, len(inner_dict_contents)/2):
-  SONGS[inner_dict_contents[2*n].text] = song_name(inner_dict_contents[2*n + 1])
+	print "successful."
+except:
+	sys.exit("something went wrong. You might want to check the value of XML_LINE_WHERE_TRACKS_START")
 
-for s in SONGS:
-	print SONGS[s]
+# Parse playlists
+print "Attempting to parse playlists..."
+XML_LINE_WHERE_PLAYLISTS_START = XML_LINE_WHERE_TRACKS_START + 2
+# try:
+playlist_dict_contents = dict_contents[XML_LINE_WHERE_PLAYLISTS_START]
+PLAYLISTS = {}	# key-value map of playlist name to list of tracks
+
+for n in range(len(playlist_dict_contents)):
+	print playlist_name(playlist_dict_contents[n])
+	# PLAYLISTS[playlist_name(playlist_dict_contents[2*n])] = playlist_items(playlist_dict_contents[2*n])
+
+# except:
+# sys.exit("something went wrong. You might want to check the value of XML_LINE_WHERE_PLAYLISTS_START")
+
+
+
+
